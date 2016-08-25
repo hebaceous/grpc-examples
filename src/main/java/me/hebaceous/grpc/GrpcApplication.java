3ebfed573f28ec47b1b8a35860ad1cd687ecdc0a
Hebaceous <hebaceous@gmail.com>
2016-08-25 16:31:48 +0800

    LockSupport.park()

--- src/main/java/me/hebaceous/grpc/GrpcApplication.java
+++ src/main/java/me/hebaceous/grpc/GrpcApplication.java
@@ -3,10 +3,13 @@ package me.hebaceous.grpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.locks.LockSupport;
+
 @SpringBootApplication
 public class GrpcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcApplication.class, args);
+        LockSupport.park();
 	}
 }
